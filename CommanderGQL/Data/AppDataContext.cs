fc0faf19b263df4d1d2c298928a3812cc1a7397e
Piotr Kowalski <Kowalskii1@op.pl>
2021-10-24 23:18:45 +0200

    Add gitignore, refactor sql and ef core config

---
+++ CommanderGQL/Data/AppDataContext.cs
@@ -0,0 +1,15 @@
+﻿using CommanderGQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderGQL.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
